ac38189518082f0df646fd1452b40231ff32da0b
VictorAVV <46534051+VictorAVV@users.noreply.github.com>
2019-03-18 01:16:58 +0200

    список статей в виде таблицы.
    Отображаение даты статьи

--- articlesMvc/view/blog.php
+++ articlesMvc/view/blog.php
@@ -4,15 +4,26 @@ include 'header.php';
 ?>
 
 <div class="jumbotron text-center">
-    <h3>Список статей</h3>
+	<h3>Список статей</h3>
 </div>
 
 <div class="container">
-	<ul>
-		<?php foreach ($articles as $article) { ?>
-		<li><a href="?page=article&id=<?=$article['id'];?>"><?=$article['title'] ?></li>
-		<?php } ?>
-	</ul>
+	<table class="table table-striped">
+		<thead>
+			<tr class="d-flex">
+				<th class="col-10" scope="col"><a href="?page=blog&sortby=title&direction=asc">Статья <i class="fa fa-sort-desc"></i></a></th>
+				<th class="col-2" scope="col"><a href="?page=blog&sortby=date&direction=asc">Дата <i class="fa fa-sort-asc"></i></a></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($articles as $article) { ?>
+			<tr class="d-flex">
+				<td class="col-10"><a href="?page=article&id=<?=$article['id'];?>"><?=$article['title']; ?></a></td>
+				<td class="col-2"><?=$article['date']; ?></td>
+			</tr>
+			<?php } ?>
+		</tbody>
+	</table>
 </div>
 
 <?php include 'footer.php'; ?>
--- articlesMvc/view/header.php
+++ articlesMvc/view/header.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./view/css/bootstrap.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 
 <body>
